eeaf1ffcc99eb983cc2ed1a1ce51f2b2dc0dbe07
Besttdk <Besttdk@ASSELBY>
2017-09-11 03:55:09 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication10/Program.cs
@@ -0,0 +1,27 @@
+﻿/*1.	С помощью рефлексии получить список методов класса Console и вывести на экран.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(Console);
+            MethodInfo[] myArrayMethodInfo = type.GetMethods(BindingFlags.Public| BindingFlags.Static|BindingFlags.NonPublic);
+            for (int i = 0; i < myArrayMethodInfo.Length; i++)
+            {
+                MethodInfo myMethodInfo = (MethodInfo)myArrayMethodInfo[i];
+                Console.WriteLine("\nThe name of the method is {0}.", myMethodInfo.Name);
+              
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿/*2.	Описать класс с несколькими свойствами. Создать экземпляр класса и инициализировать его свойства.
+ *  С помощью рефлексии получить свойства и их значения из созданного экземпляра класса. Вывести полученные значения на экран*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reflection reflection = new Reflection();
+            reflection.Age = 13;
+            reflection.Name = "Assel";
+            reflection.Id = 951;
+            Type type = typeof(Reflection);
+            FieldInfo[] myArrayOfFields = type.GetFields(BindingFlags.NonPublic| BindingFlags.Public|BindingFlags.Instance);
+            for (int i = 0; i < myArrayOfFields.Length; i++)
+            {
+                FieldInfo name = (FieldInfo)myArrayOfFields[i];
+                Console.WriteLine("{0}", name.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
